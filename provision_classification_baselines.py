fe435a9f38f3bc350dae5133878a1af149675834
Don <don.tuggener@gmail.com>
2019-09-09 12:10:32 +0200

    Refactor; move shared functions to utils.py

--- provision_classification_baselines.py
+++ provision_classification_baselines.py
@@ -1,38 +1,24 @@
-import pdb
 import numpy; numpy.random.seed(42)
-import re
-from typing import List, Set, DefaultDict, Dict, Any
-from collections import defaultdict
+from typing import List, Dict, Tuple
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.linear_model import LogisticRegression
+from utils import split_corpus, DataSet, evaluate_multilabels
 
 
-
-def train_classifiers(train_x: List[str], train_y: List[Set[str]], tf_idf: TfidfVectorizer, mlb: MultiLabelBinarizer) \
-        -> OneVsRestClassifier:
-    """
-    Train a one-vs-rest classifier per label
-    :param train_x: List of texts
-    :param train_y: List of labels
-    :param tf_idf: Fitted TF IDF transformer
-    :param mlb: Fitted MultiLabelBinarizer
-    :return: Trained classifier
-    """
+def train_classifiers(x_train: numpy.array, y_train: numpy.array) -> OneVsRestClassifier:
     clf = LogisticRegression(class_weight='balanced', max_iter=10000, solver='lbfgs')
     ovr = OneVsRestClassifier(clf, n_jobs=-1)
-    y = mlb.transform(train_y)
-    x = tf_idf.transform(train_x)
-    ovr.fit(x, y)
+    ovr.fit(x_train, y_train)
     return ovr
 
 
-def stringify_labels(y_pred_bin:numpy.array, mlb:MultiLabelBinarizer,
+def stringify_labels(y_vecs: numpy.array, mlb: MultiLabelBinarizer,
                      thresh: float = 0.5, label_threshs: Dict[str, float] = None) -> List[List[str]]:
     """
     Turn prediction probabilities into label strings
-    :param y_pred_bin:
+    :param y_vecs:
     :param mlb:
     :param thresh:
     :param label_threshs: Classification threshold per label
@@ -42,7 +28,7 @@ def stringify_labels(y_pred_bin:numpy.array, mlb:MultiLabelBinarizer,
     if not label_threshs:
         label_threshs = {l: thresh for l in mlb.classes_}
     label_threshs = [label_threshs[l] for l in mlb.classes_]
-    for prediction in y_pred_bin:
+    for prediction in y_vecs:
         label_indexes = numpy.where(prediction >= label_threshs)[0]
         if label_indexes.size > 0:  # One of the classes has triggered
             labels = set(numpy.take(mlb.classes_, label_indexes))
@@ -52,85 +38,7 @@ def stringify_labels(y_pred_bin:numpy.array, mlb:MultiLabelBinarizer,
     return y_pred
 
 
-def evaluate_multilabels(y: List[List[str]], y_preds: List[List[str]], do_print: bool = False) -> DefaultDict[str, Dict[str, float]]:
-    """
-    Print classification report with multilabels
-    :param y: Gold labels
-    :param y_preds: Predicted labels
-    :param do_print: Whether to print results
-    :return: Dict of scores per label and overall
-    """
-    # Label -> TP/FP/FN -> Count
-    label_eval: DefaultDict[str, DefaultDict[str, int]] = defaultdict(lambda: defaultdict(int))
-    assert len(y) == len(y_preds), "List of predicted and gold labels are of unequal length"
-    for y_true, y_pred in zip(y, y_preds):
-        for label in y_true:
-            if label in y_pred:
-                label_eval[label]['tp'] += 1
-            else:
-                label_eval[label]['fn'] += 1
-        for label in y_pred:
-            if label not in y_true:
-                label_eval[label]['fp'] += 1
-
-    max_len = max([len(l) for l in label_eval.keys()])
-    if do_print:
-        print('\t'.join(['Label'.rjust(max_len, ' '), 'Prec'.ljust(4, ' '), 'Rec'.ljust(4, ' '), 'F1'.ljust(4, ' '),
-                     'Support']))
-
-    eval_results: DefaultDict[str, Dict[str, float]] = defaultdict(lambda: defaultdict(float))
-    all_f1, all_rec, all_prec = [], [], []
-    for label in sorted(label_eval.keys()):
-        cnts = label_eval[label]
-        if not cnts['tp'] == 0:
-            prec = cnts['tp'] / (cnts['tp'] + cnts['fp'])
-            rec = cnts['tp'] / (cnts['tp'] + cnts['fn'])
-            f1 = 2 * prec * rec / (prec + rec)
-        else:
-            prec, rec, f1 = 0.00, 0.00, 0.00
-        eval_results[label]['prec'] = prec
-        eval_results[label]['rec'] = rec
-        eval_results[label]['f1'] = f1
-        eval_results[label]['support'] = cnts['tp'] + cnts['fn']
-        all_f1.append(f1)
-        all_rec.append(rec)
-        all_prec.append(prec)
-        if do_print:
-            print('\t'.join([label.rjust(max_len, ' '),
-                         ('%.2f' % round(prec, 2)).ljust(4, ' '),
-                         ('%.2f' % round(rec, 2)).ljust(4, ' '),
-                         ('%.2f' % round(f1, 2)).ljust(4, ' '),
-                         str(cnts['tp'] + cnts['fn']).rjust(5, ' ')]))
-
-    eval_results['Macro']['prec'] = sum(all_prec) / len(all_prec)
-    eval_results['Macro']['rec'] = sum(all_rec) / len(all_rec)
-    eval_results['Macro']['f1'] = sum(all_f1) / len(all_f1)
-    eval_results['Macro']['support'] = len(y)
-
-    # Micro
-    all_tp = sum(label_eval[label]['tp'] for label in label_eval)
-    all_fp = sum(label_eval[label]['fp'] for label in label_eval)
-    all_fn = sum(label_eval[label]['fn'] for label in label_eval)
-    eval_results['Micro']['prec'] = all_tp / (all_tp + all_fp)
-    eval_results['Micro']['rec'] = all_tp / (all_tp + all_fn)
-    micro_prec = eval_results['Micro']['prec']
-    micro_rec = eval_results['Micro']['rec']
-    eval_results['Micro']['f1'] = 2 * (micro_rec * micro_prec) / (micro_rec + micro_prec)
-    eval_results['Micro']['support'] = len(y)
-
-    if do_print:
-        print('Macro Avg. Rec:', round(eval_results['Macro']['rec'], 2))
-        print('Macro Avg. Prec:', round(eval_results['Macro']['prec'], 2))
-        print('Macro F1:', round(eval_results['Macro']['f1'], 2))
-        print()
-        print('Micro Avg. Rec:', round(eval_results['Micro']['rec'], 2))
-        print('Micro Avg. Prec:',  round(eval_results['Micro']['prec'], 2))
-        print('Micro F1:', round(eval_results['Micro']['f1'], 2))
-
-    return eval_results
-
-
-def classify_by_labelname(x_train, x_test, y_train, y_test):
+def classify_by_labelname(x_test: List[str], y_train: List[List[str]]) -> List[List[str]]:
     label_set = set(l for labels in y_train for l in labels)
     y_preds = []
     for x in x_test:
@@ -142,16 +50,15 @@ def classify_by_labelname(x_train, x_test, y_train, y_test):
     return y_preds
 
 
-def tune_clf_thresholds(test_x, test_y, classifier, mlb):
-    y_pred_bin: numpy.array = classifier.predict_proba(test_x)
+def tune_clf_thresholds(test_x, test_y, classifier: OneVsRestClassifier, mlb: MultiLabelBinarizer) \
+        -> Tuple[numpy.array, Dict[str, float]]:
+    y_pred_vecs = classifier.predict_proba(test_x)
     thresh_range = [t / 10.0 for t in range(1, 10)]
     all_results = dict()
-    print('Evaluating classification threholds')
-    for t in thresh_range:
-        print(t)
-        y_pred = stringify_labels(y_pred_bin, mlb, thresh=t)
+    for thresh in thresh_range:
+        y_pred = stringify_labels(y_pred_vecs, mlb, thresh=thresh)
         eval_results = evaluate_multilabels(test_y, y_pred, do_print=False)
-        all_results[t] = eval_results
+        all_results[thresh] = eval_results
 
     label_threshs: Dict[str, float] = dict()
     for label in mlb.classes_:
@@ -163,31 +70,31 @@ def tune_clf_thresholds(test_x, test_y, classifier, mlb):
                 best_f1 = curr_f1
         label_threshs[label] = best_thresh
 
-    y_pred = stringify_labels(y_pred_bin, mlb, label_threshs=label_threshs)
+    y_pred = stringify_labels(y_pred_vecs, mlb, label_threshs=label_threshs)
     return y_pred, label_threshs
 
 
 if __name__ == '__main__':
-    import json
-
-    # TODO try classifying sparse labels! compare results to frequent labels etc.
     corpus_file = 'sec_corpus_2016-2019_clean_sampled.jsonl'
 
-
+    print('Loading corpus', corpus_file)
+    dataset: DataSet = split_corpus(corpus_file)
 
     print('Predicting with label names')
-    y_preds_labelnames = classify_by_labelname(x_train, x_test, y_train, y_test)
-    evaluate_multilabels(y_test, y_preds_labelnames, do_print=True)
+    y_preds_labelnames = classify_by_labelname(dataset.x_train, dataset.y_train)
+    evaluate_multilabels(dataset.y_test, y_preds_labelnames, do_print=True)
 
-    mlb = MultiLabelBinarizer().fit(y)
     print('Vectorizing')
-    tfidf = TfidfVectorizer(sublinear_tf=True).fit(x_train)
-    print('Training LogReg')
-    classifier = train_classifiers(x_train, y_train, tfidf, mlb)
+    tfidfizer = TfidfVectorizer(sublinear_tf=True)
+    x_train_vecs = tfidfizer.fit_transform(dataset.x_train)
+    x_test_vecs = tfidfizer.transform(dataset.x_test)
+    mlb = MultiLabelBinarizer().fit(dataset.y_train + dataset.y_test + dataset.y_dev)
+    y_train_vecs = mlb.transform(dataset.y_train)
+    y_test_vecs = mlb.transform(dataset.y_test)
 
-    x_test = tfidf.transform(x_test)
-    y_preds_lr, _ = tune_clf_thresholds(x_test, y_test, classifier, mlb)
-    # y_preds_bin = classifier.predict_proba(x_test)
-    # y_preds_lr = stringify_labels(y_preds_bin, mlb)
-    evaluate_multilabels(y_test, y_preds_lr, do_print=True)
+    print('Training LogReg')
+    classifier = train_classifiers(x_train_vecs, y_train_vecs)
+
+    y_preds_lr, _ = tune_clf_thresholds(x_test_vecs, dataset.y_test, classifier, mlb)
+    evaluate_multilabels(dataset.y_test, y_preds_lr, do_print=True)
 
--- utils.py
+++ utils.py
@@ -1,36 +1,129 @@
 import json
-from typing import List, Union
+from typing import List, Union, Dict, DefaultDict
+from collections import defaultdict
+from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 
 
-# TODO use 3.7 dataclass for this
-class DataSet():
-    def __init__(self, x: List[str], y: List[List[str]], use_dev: bool = True):
-        x: List[str] = []
-        y: List[List[str]] = []
-        doc_ids: List[str] = []
+@dataclass
+class DataSet:
+    x_train: List[str]
+    y_train: List[List[str]]
+    x_test: List[str]
+    y_test: List[List[str]]
+    x_dev: Union[List[str], None]
+    y_dev: Union[List[List[str]], None]
 
-        for line in open(corpus_file):
-            labeled_provision = json.loads(line)
-            x.append(labeled_provision['provision'])
-            y.append(labeled_provision['label'])
-            doc_ids.append(labeled_provision['source'])
 
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-        if use_dev:
-            x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.1, random_state=42)
+def split_corpus(corpus_file: str, use_dev: bool = True,
+                 test_size: float = 0.2, dev_size: Union[float, None] = 0.1,
+                 random_state: int = 42) -> DataSet:
+    x: List[str] = []
+    y: List[List[str]] = []
+    doc_ids: List[str] = []
+    for line in open(corpus_file):
+        labeled_provision = json.loads(line)
+        x.append(labeled_provision['provision'])
+        y.append(labeled_provision['label'])
+        doc_ids.append(labeled_provision['source'])
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y,
+                                                        test_size=test_size,
+                                                        random_state=random_state)
+
+    if use_dev:
+        x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train,
+                                                          test_size=dev_size,
+                                                          random_state=random_state)
+    else:
+        x_dev, y_dev = None, None
+
+    dataset = DataSet(x_train, y_train, x_test, y_test, x_dev, y_dev)
+    return dataset
+
+
+def evaluate_multilabels(y: List[List[str]], y_preds: List[List[str]],
+                         do_print: bool = False) -> DefaultDict[str, Dict[str, float]]:
+    """
+    Print classification report with multilabels
+    :param y: Gold labels
+    :param y_preds: Predicted labels
+    :param do_print: Whether to print results
+    :return: Dict of scores per label and overall
+    """
+    # Label -> TP/FP/FN -> Count
+    label_eval: DefaultDict[str, DefaultDict[str, int]] = defaultdict(lambda: defaultdict(int))
+    assert len(y) == len(y_preds), "List of predicted and gold labels are of unequal length"
+    for y_true, y_pred in zip(y, y_preds):
+        for label in y_true:
+            if label in y_pred:
+                label_eval[label]['tp'] += 1
+            else:
+                label_eval[label]['fn'] += 1
+        for label in y_pred:
+            if label not in y_true:
+                label_eval[label]['fp'] += 1
+
+    max_len = max([len(l) for l in label_eval.keys()])
+    if do_print:
+        print('\t'.join(['Label'.rjust(max_len, ' '), 'Prec'.ljust(4, ' '), 'Rec'.ljust(4, ' '), 'F1'.ljust(4, ' '),
+                     'Support']))
+
+    eval_results: DefaultDict[str, Dict[str, float]] = defaultdict(lambda: defaultdict(float))
+    all_f1, all_rec, all_prec = [], [], []
+    for label in sorted(label_eval.keys()):
+        cnts = label_eval[label]
+        if not cnts['tp'] == 0:
+            prec = cnts['tp'] / (cnts['tp'] + cnts['fp'])
+            rec = cnts['tp'] / (cnts['tp'] + cnts['fn'])
+            f1 = 2 * prec * rec / (prec + rec)
         else:
-            x_dev, y_dev = None, None
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_test = x_test
-        self.y_test = y_test
-        self.x_dev = x_dev
-        self.y_dev = y_dev
+            prec, rec, f1 = 0.00, 0.00, 0.00
+        eval_results[label]['prec'] = prec
+        eval_results[label]['rec'] = rec
+        eval_results[label]['f1'] = f1
+        eval_results[label]['support'] = cnts['tp'] + cnts['fn']
+        all_f1.append(f1)
+        all_rec.append(rec)
+        all_prec.append(prec)
+        if do_print:
+            print('\t'.join([label.rjust(max_len, ' '),
+                         ('%.2f' % round(prec, 2)).ljust(4, ' '),
+                         ('%.2f' % round(rec, 2)).ljust(4, ' '),
+                         ('%.2f' % round(f1, 2)).ljust(4, ' '),
+                         str(cnts['tp'] + cnts['fn']).rjust(5, ' ')]))
+
+    eval_results['Macro']['prec'] = sum(all_prec) / len(all_prec)
+    eval_results['Macro']['rec'] = sum(all_rec) / len(all_rec)
+    eval_results['Macro']['f1'] = sum(all_f1) / len(all_f1)
+    eval_results['Macro']['support'] = len(y)
+
+    # Micro
+    all_tp = sum(label_eval[label]['tp'] for label in label_eval)
+    all_fp = sum(label_eval[label]['fp'] for label in label_eval)
+    all_fn = sum(label_eval[label]['fn'] for label in label_eval)
+    eval_results['Micro']['prec'] = all_tp / (all_tp + all_fp)
+    eval_results['Micro']['rec'] = all_tp / (all_tp + all_fn)
+    micro_prec = eval_results['Micro']['prec']
+    micro_rec = eval_results['Micro']['rec']
+    eval_results['Micro']['f1'] = 2 * (micro_rec * micro_prec) / (micro_rec + micro_prec)
+    eval_results['Micro']['support'] = len(y)
+
+    if do_print:
+        print('Macro Avg. Rec:', round(eval_results['Macro']['rec'], 2))
+        print('Macro Avg. Prec:', round(eval_results['Macro']['prec'], 2))
+        print('Macro F1:', round(eval_results['Macro']['f1'], 2))
+        print()
+        print('Micro Avg. Rec:', round(eval_results['Micro']['rec'], 2))
+        print('Micro Avg. Prec:',  round(eval_results['Micro']['prec'], 2))
+        print('Micro F1:', round(eval_results['Micro']['f1'], 2))
+
+    return eval_results
 
 
-
-def split_data(corpus_file: str, use_dev: bool=True):
-
+if __name__ == '__main__':
+    corpus_file = 'sec_corpus_2016-2019_clean_sampled.jsonl'
+    ds = split_corpus(corpus_file)
+    breakpoint()
 
 
