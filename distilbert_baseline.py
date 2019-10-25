7f6ba52c7c9014e7d609d2448db336fafa9e3ac0
vode <vode@zhaw.ch>
2019-10-25 12:48:24 +0200

    send class_weights to device

--- distilbert_baseline.py
+++ distilbert_baseline.py
@@ -104,7 +104,7 @@ def train(train_dataset, model, train_params, class_weights=None):
     )
 
     if class_weights is not None:
-        class_weights = torch.from_numpy(class_weights).double()
+        class_weights = torch.from_numpy(class_weights).double().to(device)
 
     no_decay = {'bias', 'LayerNorm.weight'}
     optimizer_grouped_parameters = [
