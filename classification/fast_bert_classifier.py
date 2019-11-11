983be1d4e1e8e619daf295968ec385df051ec450
Tuggener Don <tuge@gpulogin.cloudlab.zhaw.ch>
2019-11-11 16:20:49 +0100

    Update fast_bert

--- classification/fast_bert_classifier.py
+++ classification/fast_bert_classifier.py
@@ -1,4 +1,15 @@
-# https://github.com/kaushaltrivedi/fast-bert
+"""
+from https://github.com/kaushaltrivedi/fast-bert
+
+needs nvidia/apex
+
+git clone https://www.github.com/nvidia/apex
+cd apex
+python setup.py install
+
+run with
+SINGULARITYENV_LANG="C.UTF-8" srun --pty --ntasks=1 --cpus-per-task=4 --mem=32G --gres=gpu:1 singularity shell --cleanenv ~/pytorch-latest.simg
+"""
 
 import os
 import logging
@@ -12,7 +23,7 @@ from utils import split_corpus, SplitDataSet, evaluate_multilabels, \
 
 
 def write_csv(x, y, label_set_list, outfile):
-    with open(outfile, 'w') as f:
+    with open(outfile, 'w', encoding='utf-8') as f:
         f.write("text, " + ','.join(label_set_list) + '\n')
         for sample, labels in zip(x, y):
             f.write('"' + sample.replace('\t', ' ').replace('"', "'") + '"')
@@ -22,16 +33,18 @@ def write_csv(x, y, label_set_list, outfile):
 
 
 if __name__ == '__main__':
+    DATA_PATH = 'data/'
+    LABEL_PATH = 'data/'
+    OUTPUT_DIR = 'data/'
+    logger = logging.getLogger()
+    device_cuda = torch.device("cuda")
+    metrics = [{'name': 'accuracy', 'function': accuracy}]
+
+    """
     corpus_file = 'data/sec_corpus_2016-2019_clean_NDA_PTs2.jsonl'
     classifier_file = 'saved_models/fastbert_nda.pkl'
     # corpus_file = 'data/sec_corpus_2016-2019_clean_proto.jsonl'
     # classifier_file = 'saved_models/fastbert_proto.pkl'
-
-    logger = logging.getLogger()
-    # device_cuda = torch.device('cuda' if torch.cuda.is_avaliable() else 'cpu')
-    # device_cuda = 'cpu'
-    device_cuda = torch.device("cuda")
-    metrics = [{'name': 'accuracy', 'function': accuracy}]
 
     print('Loading corpus from', corpus_file)
     dataset: SplitDataSet = split_corpus(corpus_file)
@@ -46,9 +59,7 @@ if __name__ == '__main__':
     test_y = mlb.transform(dataset.y_train)
     dev_y = mlb.transform(dataset.y_dev)
 
-    DATA_PATH = 'data/'
-    LABEL_PATH = 'data/'
-    OUTPUT_DIR = 'data/'
+
 
     # create the CSVs
     write_csv(dataset.x_train, train_y, list(mlb.classes_), os.path.join(DATA_PATH, 'train.csv'))
@@ -58,23 +69,26 @@ if __name__ == '__main__':
     with open(os.path.join(LABEL_PATH, 'labels.csv'), 'w') as f:
         for label in label_set:
             f.write(label + '\n')
+    """
 
+    label_cols = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
     databunch = BertDataBunch(DATA_PATH, LABEL_PATH,
-                              tokenizer='bert-base-uncased',
-                              train_file='train.csv',
-                              val_file='test.csv',
+                              tokenizer='distilbert-base-uncased',
+                              train_file='train_sample.csv',
+                              val_file='val_sample.csv',
                               label_file='labels.csv',
                               text_col='text',
-                              label_col=list(mlb.classes_),
+                              #label_col=list(mlb.classes_),
+                              label_col=label_cols,
                               batch_size_per_gpu=16,
                               max_seq_length=512,
                               multi_gpu=False,
                               multi_label=True,
-                              model_type='bert')
+                              model_type='distilbert')
 
     learner = BertLearner.from_pretrained_model(
         databunch,
-        pretrained_path='bert-base-uncased',
+        pretrained_path='distilbert-base-uncased',
         metrics=metrics,
         device=device_cuda,
         logger=logger,
@@ -91,5 +105,7 @@ if __name__ == '__main__':
                 validate=True,  # Evaluate the model after each epoch
                 schedule_type = "warmup_cosine",
                 optimizer_type = "lamb")
+
+    learner.validate()
 
     learner.save_model()
